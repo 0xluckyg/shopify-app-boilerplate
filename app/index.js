b6768f4ec50e42242e724ba67240228828536cd6
Scott Cho <scottsgcho@gmail.com>
2018-10-13 23:58:13 +0900

    format user server

--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@ require('./db/mongoose');
 const bodyParser = require('body-parser');
 let express = require("express");
 const http = require('http');
+const user = require('./server/user')
 
 const app = express();
 const port = process.env.PORT;
@@ -19,41 +20,7 @@ app.use(function(req, res, next) {
 
 app.use(bodyParser.json());
 
-app.post('/user/login', (req, res) => {
-    const body = _.pick(req.body, ['email', 'password']);
 
-    User.findByCredentials(body.email, body.password).then((user) => {
-        return user.generateAuthToken().then((token) => {
-            res.send({ token, user });
-        });
-    }).catch((err) => {
-        res.status(400).send(err);
-    });
-});
-
-app.post('/user/signup', (req, res) => {
-    const user = new User(req.body);
-
-    user.save().then(() => {
-        return user.generateAuthToken();
-    }).then(token => {
-        res.send({ token, user });
-    }).catch((err) => {
-        res.status(400).send(err);
-    });
-});
-
-app.get('/user/me', (req, res) => {
-    const queryToken = req.query.token;
-    
-    User.findByToken(queryToken)
-    .then(user => {
-        user.removeToken(queryToken);
-        return user.generateAuthToken().then((token) => {
-            res.send({ token, user });
-        });
-    });
-});
 
 server.listen(port, () => {
     console.log('Started on port: ', port);
