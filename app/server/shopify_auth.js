6f7dd0c6d73e6c36c382ae0abbe1a133a1ce6fad
Scott Cho <scottsgcho@gmail.com>
2018-10-16 23:50:18 +0900

    shopify auth scopes

--- app/server/shopify_auth.js
+++ app/server/shopify_auth.js
@@ -1,12 +1,46 @@
+// Shopify Partners Login
+// https://partners.shopify.com
+// OAuth Doc
+// https://help.shopify.com/en/api/getting-started/authentication/oauth
+// Shopify Permissions Scope
+// https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
 
+const crypto = require("crypto");
+const shopifyScopes = [
+    "read_products",
+    "write_products",
+    "read_product_listings",
+    "read_customers",
+    "write_customers",
+    "read_orders",
+    "write_orders",
+    // "read_all_orders", //Requires approval from Shopify Partners dashboard
+    "read_draft_orders",
+    "write_draft_orders",
+    "read_inventory",
+    "write_inventory",
+    "read_shipping",
+    "write_shipping",
+    "read_analytics",
+    "read_users",
+    "write_users",
+    "read_marketing_events",
+    "write_marketing_events",
+    "read_resource_feedbacks",
+    "write_resource_feedbacks",
+    "read_shopify_payments_payouts",
+    "unauthenticated_read_product_listings",
+    "unauthenticated_write_checkouts",
+    "unauthenticated_write_customers"
+]
 
-let shop = ""
-let api_key = process.env.SHOPIFY_API_KEY
-let scopes = "write_orders,read_customers"
-let redirect_url = ""
-let nonce = "A randomly selected value provided by your application, which is unique for each authorization request. During the OAuth callback phase, your application must check that this value matches the one you provided during authorization. This mechanism is important for the security of your application."
-let option = ""
+function formatURL(shopName) {
+    let api_key = process.env.SHOPIFY_API_KEY
+    let scopes = shopifyScopes.join()
+    let redirect_url = ""
+    const nonce = crypto.randomBytes(16).toString("hex")
 
-let authUrl = `https://${shop}.myshopify.com/admin/oauth/authorize?client_id=${api_key}&scope=${scopes}&redirect_uri=${redirect_uri}&state=${nonce}&grant_options[]=${option}`
+    let authUrl = `https://${shopName}.myshopify.com/admin/oauth/authorize?client_id=${api_key}&scope=${scopes}&redirect_uri=${redirect_uri}&state=${nonce}`
+    return authUrl
+}
 
-'https://help.shopify.com/en/api/getting-started/authentication/oauth'
