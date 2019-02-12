d7a261a31ea7b3b6f658483e6c21789a9fb4e8c9
Scott Cho <scottsgcho@gmail.com>
2019-02-12 23:17:05 +0900

    new hash auth

--- app/db/mongoose.js
+++ app/db/mongoose.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 //Tells mongoose we're using bluebird promise
 mongoose.Promise = require('bluebird');
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI, {
+    useMongoClient: true
+});
 
 module.exports = {mongoose};
--- app/index.js
+++ app/index.js
@@ -3,8 +3,17 @@ require('./db/mongoose');
 const bodyParser = require('body-parser');
 let express = require("express");
 const http = require('http');
-const user = require('./server/user')
-const shopifyAuth = require('./server/shopify_auth')
+
+const crypto = require('crypto');
+const cookie = require('cookie');
+const nonce = require('nonce')();
+const querystring = require('querystring');
+// const request = require('request-promise');
+
+const apiKey = process.env.SHOPIFY_API_KEY;
+const apiSecret = process.env.SHOPIFY_API_SECRET;
+const scopes = 'read_products';
+const forwardingAddress = "https://af6bebbc.ngrok.io";
 
 const app = express();
 const port = process.env.PORT;
@@ -21,12 +30,71 @@ app.use(function(req, res, next) {
 
 app.use(bodyParser.json());
 
-user.login(app)
-user.signup(app)
-user.getUser(app)
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
-shopifyAuth.addShopify(app)
+app.get('/shopify', (req, res) => {
+    const shop = req.query.shop;
+    if (shop) {
+      const state = nonce();
+      const redirectUri = forwardingAddress + '/shopify/callback';
+      const installUrl = 'https://' + shop +
+        '/admin/oauth/authorize?client_id=' + apiKey +
+        '&scope=' + scopes +
+        '&state=' + state +
+        '&redirect_uri=' + redirectUri;
+  
+      res.cookie('state', state);
+      res.redirect(installUrl);
+    } else {
+      return res.status(400).send('Missing shop parameter. Please add ?shop=your-development-shop.myshopify.com to your request');
+    }
+});
 
+app.get('/shopify/callback', (req, res) => {
+    const { shop, hmac, code, state } = req.query;
+    const stateCookie = cookie.parse(req.headers.cookie).state;
+  
+    if (state !== stateCookie) {
+      return res.status(403).send('Request origin cannot be verified');
+    }
+  
+    if (shop && hmac && code) {        
+        // TODO
+        // Validate request is from Shopify
+        const map = Object.assign({}, req.query);
+        delete map['signature'];
+        delete map['hmac'];
+        const message = querystring.stringify(map);
+        const providedHmac = Buffer.from(hmac, 'utf-8');
+        const generatedHash = Buffer.from(
+        crypto
+            .createHmac('sha256', apiSecret)
+            .update(message)
+            .digest('hex'),
+            'utf-8'
+        );
+        let hashEquals = false;
+        // timingSafeEqual will prevent any timing attacks. Arguments must be buffers
+        
+        try {
+            hashEquals = crypto.timingSafeEqual(generatedHash, providedHmac)
+            // timingSafeEqual will return an error if the input buffers are not the same length.
+        } catch (e) {
+            hashEquals = false;
+        };
+
+        if (!hashEquals) {
+            return res.status(400).send('HMAC validation failed');
+        }
+
+        res.status(200).send('HMAC validated');
+
+    } else {
+        res.status(400).send('Required parameters missing');
+    }
+});
 
 server.listen(port, () => {
     console.log('Started on port: ', port);
